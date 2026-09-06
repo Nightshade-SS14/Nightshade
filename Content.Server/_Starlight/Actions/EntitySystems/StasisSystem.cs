9c15133da1ae1e3f912a5c645490ab06e4392bfd
DOPEy187 <dopeygnomes@gmail.com>
2026-09-06 22:01:58 +0100

    Fix Avali stasis body healing

--- Content.Server/_Starlight/Actions/EntitySystems/StasisSystem.cs
+++ Content.Server/_Starlight/Actions/EntitySystems/StasisSystem.cs
@@ -54,7 +54,14 @@ public sealed partial class StasisSystem : SharedStasisSystem
             var modifier = _mobState.IsCritical(uid) ? -comp.CritHealingModifier : -1.0f;
             var oldDamageCompTotal = _damageable.GetTotalDamage((uid, damageComponent));
 
-            _damageable.TryChangeDamage(uid, modifier * comp.HealingPerUpdate, true, origin: uid);
+            _damageable.TryChangeDamage(
+                uid,
+                modifier * comp.HealingPerUpdate,
+                true,
+                origin: uid,
+                targetPart: TargetBodyPart.All,
+                ignoreBlockers: true,
+                splitDamage: SplitDamageBehavior.SplitEnsureAllDamagedAndOrganic);
 
             _bloodstream.TryModifyBleedAmount(uid, modifier * comp.BleedHealPerUpdate);
 
